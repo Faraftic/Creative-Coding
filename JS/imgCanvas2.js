a84ec8b4c2fad53b521cc827029741f8b1117d45
Faraftic <nils.chenet@gmail.com>
2025-04-07 09:42:23 +0200

    Completed Image Merge task and added few more pictures

--- JS/imgCanvas2.js
+++ JS/imgCanvas2.js
@@ -1,28 +1,70 @@
 let img;
 let img2;
-
 function preload() {
-  img2 = loadImage("/Images/lebron02.jpg");
-  img = loadImage("/Images/lebron01.jpg");
+  img2 = loadImage("../Images/bomboclat.jpeg");
+  // img2 = loadImage("../Images/lebron02.jpg");
+  img = loadImage("../Images/lebron01.jpg");
+  // img2 = loadImage("../Images/bombardino.jpeg");
+  // img1 = loadImage("../Images/tralalero.jpeg");
+}
+function setup() {
+  createCanvas(1200, 600);
+  // pixelDensity(1);
+
+  img2.loadPixels();
+  img.loadPixels();
+
+  newImg = createImage(img.width, img.height);
+  newImg.loadPixels();
+
+  for (let i = 0; i <= img.pixels.length; i += 8) {
+    newImg.pixels[i] = img2.pixels[i];
+    newImg.pixels[i + 1] = img2.pixels[i + 1];
+    newImg.pixels[i + 2] = img2.pixels[i + 2];
+    newImg.pixels[i + 3] = img2.pixels[i + 3];
+
+    newImg.pixels[i + 4] = img.pixels[i + 4];
+    newImg.pixels[i + 5] = img.pixels[i + 5];
+    newImg.pixels[i + 6] = img.pixels[i + 6];
+    newImg.pixels[i + 7] = img.pixels[i + 7];
+  }
+  newImg.updatePixels();
+  image(newImg, 100, 100, 900, 400);
 }
 
+/*
+
+let img;
+let img2;
+function preload() {
+  img2 = loadImage("../Images/lebron02.jpg");
+  img = loadImage("../Images/lebron01.jpg");
+}
 function setup() {
   createCanvas(1200, 600);
   pixelDensity(1);
-  image(img, 100, 100, 900, 400);
 
-  for (let i = 0; i <= 900; i++) {
-    if (true) {
-      img.loadPixels();
-      for (let y = 0; y < img.height; y += 2) {
-        for (let x = 0; x < img.width; x += 2) {
-          let index = (x + y * img.width) * 4;
-        }
-      }
-      img.updatePixels();
+  img2.loadPixels();
+  img.loadPixels();
+
+  newImg = createImage(img.width, img.height);
+  newImg.loadPixels();
+
+  for (let i = 0; i <= img.pixels.length; i += 4) {
+    let pixelIndex = i / 4;
+    if (pixelIndex % 2 == 0) {
+      newImg.pixels[i] = img2.pixels[i];
+      newImg.pixels[i + 1] = img2.pixels[i + 1];
+      newImg.pixels[i + 2] = img2.pixels[i + 2];
+      newImg.pixels[i + 3] = img2.pixels[i + 3];
     } else {
+      newImg.pixels[i] = img.pixels[i];
+      newImg.pixels[i + 1] = img.pixels[i + 1];
+      newImg.pixels[i + 2] = img.pixels[i + 2];
+      newImg.pixels[i + 3] = img.pixels[i + 3];
     }
   }
-
-  function draw() {}
+  newImg.updatePixels();
+  image(newImg, 100, 100, 900, 400);
 }
+*/
