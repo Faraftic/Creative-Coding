7504bf027f6d9cfcbe5ac581aa96364dd35d27ca
Faraftic <nils.chenet@gmail.com>
2025-04-07 08:16:39 +0200

    Started imgCanvvas 2 with 2 images as one Image

--- JS/imgCanvas2.js
+++ JS/imgCanvas2.js
@@ -9,8 +9,20 @@ function preload() {
 function setup() {
   createCanvas(1200, 600);
   pixelDensity(1);
-}
+  image(img, 100, 100, 900, 400);
 
-function draw() {
-    
+  for (let i = 0; i <= 900; i++) {
+    if (true) {
+      img.loadPixels();
+      for (let y = 0; y < img.height; y += 2) {
+        for (let x = 0; x < img.width; x += 2) {
+          let index = (x + y * img.width) * 4;
+        }
+      }
+      img.updatePixels();
+    } else {
+    }
+  }
+
+  function draw() {}
 }
